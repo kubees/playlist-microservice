a588371c6f1176adeddbdc50c084ef373630c27a
AhmedGrati <ahmedgratti@think-it.io>
2022-11-12 20:15:18 +0100

    chore: add handlers file

--- app.go
+++ app.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/go-redis/redis/v8"
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
-	"io/ioutil"
-	"net/http"
-	"os"
 )
 
 var environment = os.Getenv("ENVIRONMENT")
@@ -22,46 +20,18 @@ var ctx = context.Background()
 var rdb *redis.Client
 
 func main() {
-
-	router := httprouter.New()
-
-	router.GET("/healthz", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-		fmt.Fprintf(writer, "ok")
-	})
-
-	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		cors(w)
-		playlistsJson := GetPlaylists()
-
-		playlists := []Playlist{}
-		err := json.Unmarshal([]byte(playlistsJson), &playlists)
-		if err != nil {
-			panic(err)
-		}
-
-		//get videos for each playlist from videos api
-		for pi := range playlists {
-			vs := GetVideosOfPlaylists(playlists[pi])
-			playlists[pi].Videos = vs
-		}
-
-		playlistsBytes, err := json.Marshal(playlists)
-		if err != nil {
-			panic(err)
-		}
-
-		reader := bytes.NewReader(playlistsBytes)
-		if b, err := ioutil.ReadAll(reader); err == nil {
-			fmt.Fprintf(w, "%s", string(b))
-		}
-
-	})
-
 	r := redis.NewClient(&redis.Options{
 		Addr: redis_host + ":" + redis_port,
 		DB:   0,
 	})
 	rdb = r
+
+	router := httprouter.New()
+
+	router.GET("/healthz", HealthzHandler)
+
+	router.GET("/", GetPlaylistsHandler)
+
 
 	fmt.Println("Running...")
 	log.Fatal(http.ListenAndServe(":10010", router))
--- config.go
+++ config.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func cors(writer http.ResponseWriter) {
+func Cors(writer http.ResponseWriter) {
 	if environment == "DEBUG" {
 		writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-MY-API-Version")
